5ba77565e49a9141cc20c739dd3045341b19189c
yeonwooz <woosean999@gmail.com>
2024-02-16 00:33:48 +0900

    feat(app): add template

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 os.environ['OPENAI_API_KEY'] = apikey
 
 # App Framework
-st.title('Youtube GPT Creator')
+st.title('Writer GPT Creator')
 prompt = st.text_input('Plug in your propt here')
+
+# Template
+title_template = PromptTemplate(
+	input_variables = ['topic'],
+	template='write me a book title about {topic}'
+)
+
+#Llms
+llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
+
+# Show stuff to the screen if there's a prompt
+if prompt:
+	response = title_chain.run(topic=prompt)
+	st.write(response)
