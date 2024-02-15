b0457d50441d42134ce14fcf28d6703c64789bba
yeonwooz <woosean999@gmail.com>
2024-02-16 00:49:14 +0900

    feat(app): add SequentialChain

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ py -m pip install (모듈)
 py -m streamlit run app.py
 ```
 
-
 ## Key Concept
+
 Models - Prompts - Indexes - Memory - Chains - Agents
+
+## 참고자료
+
+https://www.youtube.com/watch?v=MlK6SIjcjE8
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -26,11 +26,12 @@ script_template = PromptTemplate(
 
 #Llm chains
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title')
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title','script'], verbose=True)
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-	response = sequential_chain.run(prompt)
-	st.write(response)
+	response = sequential_chain({'topic':prompt})
+	st.write(response['title'])
+	st.write(response['script'])
