130b468c076f36df63f96466d36cfc542ed231ec
yeonwooz <woosean999@gmail.com>
2024-02-16 01:01:06 +0900

    feat(app): add wiki research

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
+from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -16,29 +17,40 @@ prompt = st.text_input('Plug in your propt here')
 
 # Template
 title_template = PromptTemplate(
-	input_variables = ['topic'],
+	input_variables = ['topic', 'wikipedia_research'],
 	template='write me a book title about {topic}'
 )
 
 script_template = PromptTemplate(
-	input_variables = ['title'],
-	template='write me a short story based on this title TITLE: {title}'
+	input_variables = ['title', 'wikipedia_research'],
+	template='write me a short story based on this title TITLE: {title} while leveraging this wikipedia research:{wikipedia_research}'
 )
 
 # Memory
-memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 
 #Llm chains
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=memory)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=memory)
-sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title','script'], verbose=True)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
+
+wiki=WikipediaAPIWrapper()
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-	response = sequential_chain({'topic':prompt})
-	st.write(response['title'])
-	st.write(response['script'])
+	title=title_chain.run(prompt)
+	wiki_research=wiki.run(prompt)
+	script=script_chain.run(title=title, wikipedia_research=wiki_research)
+	
+	st.write(title)
+	st.write(script)
 
-	with st.expander('Message History'):
-		st.info(memory.buffer)
+	with st.expander('Title History'):
+		st.info(title_memory.buffer)
+
+	with st.expander('Script History'):
+		st.info(script_memory.buffer)
+
+	with st.expander('Wikipedia Research'):
+		st.info(wiki_research)
